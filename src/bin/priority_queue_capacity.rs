771f9ad6e9b318faedc79cbafbbe80d70070c460
Markus Jais <markusjais@gmx.de>
2015-08-05 21:37:15 +0200

    examples for capacity of binary heaps

---
+++ src/bin/priority_queue_capacity.rs
@@ -0,0 +1,23 @@
+#![feature(collections)]
+
+use std::collections::BinaryHeap;
+
+
+fn main() {
+    let mut heap_1: BinaryHeap<i32> = BinaryHeap::with_capacity(5);
+    let mut heap_2: BinaryHeap<i32> = BinaryHeap::with_capacity(5);
+
+    println!("capacity heap_1:{}", heap_1.capacity());
+    println!("capacity heap_2:{}", heap_2.capacity());
+
+    heap_1.reserve(6);
+    println!("capacity heap_1 after reserve:{}", heap_1.capacity()); // prints 12 with rust 1.3.0-dev
+
+    heap_2.reserve_exact(6);
+    println!("capacity heap_2 after reserve_exact:{}", heap_2.capacity()); // prints 6
+
+    heap_2.push(42); // now one element in heap1
+    heap_2.shrink_to_fit();
+    println!("capacity heap_2 after shrink_to_fit:{}", heap_2.capacity()); // prints 1
+
+}
