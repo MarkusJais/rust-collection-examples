02ee96c4e1774de1d5afe3060d05af9e9e216527
Markus Jais <markusjais@gmx.de>
2015-08-08 13:39:57 +0200

    better output messages, more examples, cleanup for vec_capacity

--- src/bin/vec_capacity.rs
+++ src/bin/vec_capacity.rs
@@ -1,4 +1,11 @@
+// this code shows the effects of different methods on the capcacity of Vec
+// the different examles are separated by "====================================="
+// if you find a bug please open a pull request
+
+
 fn main() {
+
+    println!("\n1) ==========================================\n");
 
     let mut vec: Vec<i32> = Vec::new();  // empty vector has capacity of 0
 
@@ -15,11 +22,32 @@ fn main() {
 
     println!("vec:{:?}", vec);
 
-    println!("\n==========================================\n");
+    println!("\n2) ==========================================\n");
+
+
 
     let mut vec: Vec<i32> = Vec::new();  // empty
     vec.push(100);  // empty vec with capacity 0 will resize to capacity of 4 when one element is added
-    println!("capacity vec after pushing 1 element:{}", vec.capacity());
+    println!("capacity vec after pushing 1 element to empty vec:{}", vec.capacity());
+
+    let mut vec: Vec<i32> = vec![1];  // capacity is 4Vec::new();  // empty
+    println!("capacity vec after macro initialization with 1 element: {}", vec.capacity());
+    vec.push(100); // non empty vectors resize capacity to double their currenty capcacity
+    println!("capacity vec after macro initialization with 1 element and pushing 1 element:{}", vec.capacity());
+
+    let mut vec: Vec<i32> = vec![1,2,3,4];  // capacity is 4
+    println!("capacity vec after macro initialization with 4 elements: {}", vec.capacity());
+    vec.push(100); // small vectors resize to a capacity of at least 4
+    println!("capacity vec after macro initialization with 4 element and pushing 1 element:{}", vec.capacity());
+
+
+    println!("\n3) ==========================================\n");
+
+
+
+    let mut vec: Vec<i32> = Vec::new();  // empty
+    vec.push(100);  // empty vec with capacity 0 will resize to capacity of 4 when one element is added
+    println!("capacity vec after pushing 1 element to empty vec:{}", vec.capacity());
     vec.reserve(8);  // 1 element in the vector + 8 reserved and then multiplied by 2. (1 + 8) * 2 = 19
     println!("capacity vec after pushing 1 element and reserve(8):{}", vec.capacity());
 
@@ -28,7 +56,10 @@ fn main() {
 
     println!("vec:{:?}", vec);  // just to prove that the element is still there after shrinking :-)
 
-    println!("\n==========================================\n");
+
+    println!("\n4) ==========================================\n");
+
+
 
     let mut vec: Vec<i32> = Vec::with_capacity(5);
     println!("capacity vec:{}", vec.capacity());
@@ -47,8 +78,10 @@ fn main() {
     vec.push(6);  // now resizing happens
     println!("capacity after adding 6 element to empty vec with capacity 5:{}", vec.capacity());
 
+    println!("\n5) ==========================================\n");
 
-    println!("\n==========================================\n");
+
+
     let mut vec: Vec<i32> = Vec::with_capacity(5);
     vec.reserve(4); // still capacity of 5
     println!("capacity of empty vec with initial capacity of 5 after reserve(4):{}", vec.capacity());
@@ -56,7 +89,10 @@ fn main() {
     vec.reserve(6); // now capacity of 12 (6 * 2)
     println!("capacity of empty vec with initial capacity of 5 after reserve(6):{}", vec.capacity());
 
-    println!("\n==========================================\n");
+    println!("\n6) ==========================================\n");
+
+
+
     let mut vec: Vec<i32> = Vec::with_capacity(5);
     vec.push(1);
     vec.push(2);
@@ -66,19 +102,27 @@ fn main() {
     vec.reserve(6); // no changes because capacity is already 12
     println!("capacity vec with initial capacity of 5 after adding 2 and reserve(4) and then reserve(6):{}", vec.capacity());
 
+    println!("\n7) ==========================================\n");
 
-    println!("\n==========================================\n");
+
+
     let mut vec: Vec<i32> = Vec::with_capacity(5);
     vec.push(1);
     vec.push(2);
     vec.reserve_exact(3); // 2 elements added and 3 reserved will keep the initial capacity of 5
     println!("capacity vec with initial capacity of 5 after adding 2 elements and reserve_exact(3):{}", vec.capacity());
 
+    println!("\n8) ==========================================\n");
 
-    println!("\n==========================================\n");
+
+
     let mut vec: Vec<i32> = Vec::with_capacity(5);
     vec.push(1);
     vec.push(2);
     vec.reserve_exact(4); // capacity now 6. 2 elements already inside the vector and reserving exactly 4 more
     println!("capacity vec with initial capacity of 5 after adding 2 elements and reserve_exact(4):{}", vec.capacity());
+
+
+
+    println!("\n Done!\n");
 }
